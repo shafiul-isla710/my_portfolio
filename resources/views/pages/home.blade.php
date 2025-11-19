1a29aef34f5796f2298c4af7aad0ae9cf49994f9
shafiul <shafiul7sms@gmail.com>
2025-11-19 15:25:42 +0600

    add home page and hero section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,13 +6,11 @@
 <!-- Hero Section -->
     <section id="hero" class="hero section">
 
-      <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
+      <img src="assets/img/portfolio1.jpg" alt="" data-aos="fade-in">
 
       <div class="container text-center" data-aos="zoom-out" data-aos-delay="100">
         <div class="row justify-content-center">
           <div class="col-lg-8">
-            <h2>Kelly Adams</h2>
-            <p>I'm a professional illustrator from San Francisco</p>
             <a href="about.html" class="btn-get-started">About Me</a>
           </div>
         </div>
