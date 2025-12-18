75137c85288d08254f5b838c62a3c630e1cbec81
shafiul <shafiul7sms@gmail.com>
2025-12-18 09:41:41 +0600

    fixed resume section

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -13,24 +13,6 @@
           <li><a href="{{ url('/about-page') }}">About</a></li>
           <li><a href="{{ url('/resume') }}">Resume</a></li>
           <li><a href="{{ url('/service') }}">Services</a></li>
-          <li><a >Portfolio</a></li>
-          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-            <ul>
-              <li><a href="#">Dropdown 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                <ul>
-                  <li><a href="#">Deep Dropdown 1</a></li>
-                  <li><a href="#">Deep Dropdown 2</a></li>
-                  <li><a href="#">Deep Dropdown 3</a></li>
-                  <li><a href="#">Deep Dropdown 4</a></li>
-                  <li><a href="#">Deep Dropdown 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Dropdown 2</a></li>
-              <li><a href="#">Dropdown 3</a></li>
-              <li><a href="#">Dropdown 4</a></li>
-            </ul>
-          </li>
           <li><a href="{{ url('/contact') }}">Contact</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
